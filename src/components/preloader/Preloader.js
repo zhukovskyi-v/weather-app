e84f3137611bdbcd9cdb4804d279ecff9e1aae0a
Владислав <shykovskiy@gmail.com>
2021-02-13 10:49:44 +0200

    Add preloader component

---
+++ src/components/preloader/Preloader.js
@@ -0,0 +1,11 @@
+import React from "react";
+import './preloader.css'
+
+export const Preloader = () => {
+    return (<div className="square">
+        <div className="circle">
+        </div>
+        <div className="circle">
+        </div>
+    </div>)
+}
